ee960d9c5a899c6ae9b539929ba7b3f009136858
tarak996 <63512296+tarak996@users.noreply.github.com>
2022-11-17 12:47:47 +0530

    Update settings.py

--- health/settings.py
+++ health/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9o@t8g$i@c6@o4yrtoyu*(!k=^%s+%2hu+m7932x#nka8vf+^g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
